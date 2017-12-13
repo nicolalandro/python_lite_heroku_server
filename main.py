281009835d5a62fcc671b9fd831cf888bb9fba28
nicolalandro <nicolaxx94@live.it>
2017-12-13 21:26:08 +0100

    creata rotta e html per caricare immagine

--- main.py
+++ main.py
@@ -9,5 +9,10 @@ def main():
     return render_template('index.html')
 
 
+@app.route('/add_supervisioned_data')
+def add_supervisioned_data():
+    return render_template('load_image.html')
+
+
 if __name__ == '__main__':
     app.run()
