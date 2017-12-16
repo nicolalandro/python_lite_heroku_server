37ac20d02292534e7304faa32b5752ef1c653939
nicolalandro <nicolaxx94@live.it>
2017-12-16 10:20:31 +0100

    click del bottone send -> log

--- static/js/image_load.js
+++ static/js/image_load.js
@@ -38,3 +38,15 @@ $(function() {
     }
   });
 });
+
+$(document).ready(function(){
+    init();
+});
+
+function init(){
+    $('#send_json').click(clickSendImage);
+}
+
+function clickSendImage(d){
+    console.log("send image...")
+}
