80fe78154331f0da218730521d3726bfc3e9d939
nicolalandro <nicolaxx94@live.it>
2017-12-16 13:22:15 +0100

    upload test.txt test file

--- src/cloud_image_loader.py
+++ src/cloud_image_loader.py
@@ -1,3 +1,4 @@
+import os
 from pcloud import PyCloud
 
 
@@ -6,4 +7,9 @@ class CloudImageLoader(object):
         self.pc = PyCloud('nicolaxx94@live.it', 'cloudperimmagini')
 
     def list_folder(self):
+        testfile = os.path.join('/home/mint', 'Scrivania', 'test.txt')
+        f = open(testfile, "w")
+        f.write("test")
+        f.close()
+        self.pc.uploadfile(files=[testfile])
         return self.pc.listfolder(folderid=0)
