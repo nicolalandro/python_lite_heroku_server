e66d9d3c4186f4ec35c55f5aabf7ba57f24a6d22
nicolalandro <nicolaxx94@live.it>
2017-12-16 11:06:37 +0100

    controlli imput specie

--- static/js/image_load.js
+++ static/js/image_load.js
@@ -8,4 +8,11 @@ function init(){
 
 function clickSendImage(d){
     console.log("send image...")
+    var species = $('#fish_specie').val();
+    if(species && species.length > 0){
+        console.log(species)
+    }
+    else {
+        console.log("No species found")
+    }
 }
