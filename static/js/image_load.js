279b3395852b44cde6a26cc0e1b8680128386f26
nicolalandro <nicolaxx94@live.it>
2017-12-16 11:27:08 +0100

    controlli corretti

--- static/js/image_load.js
+++ static/js/image_load.js
@@ -9,10 +9,27 @@ function init(){
 function clickSendImage(d){
     console.log("send image...")
     var species = $('#fish_specie').val();
-    if(species && species.length > 0){
-        console.log(species)
+    var image = $('#dropzone').find('img').attr('src');
+    if(isInputCorrect(species, image)){
+        toastSuccess("correct")
     }
-    else {
-        console.log("No species found")
+}
+
+function isInputCorrect(species, image){
+    if( !(species && species.length > 0) ){
+        toastError("No species found")
     }
+    if( !(image && image.length > 0) ) {
+        toastError("No image found")
+    }
+
+    return species && species.length > 0 && image && image.length > 0;
+}
+
+function toastSuccess(msg){
+    console.log(msg)
+}
+
+function toastError(msg){
+    console.log(msg)
 }
