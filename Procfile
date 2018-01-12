7027a04051450068aaf1dab953d1363fbb054334
nicolalandro <nicolaxx94@live.it>
2018-01-12 20:21:54 +0100

    aggiunto step in Procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
+python set_pcloud_credential.py
 web: gunicorn main:app --log-file=-
--- set_pcloud_credential.py
+++ set_pcloud_credential.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python
-import sys
 import os
 
-if len(sys.argv) > 3 | len(sys.argv) < 2:
-    print("Error: number of arguments should be two. Try to run 'python set_pcloud_credential uname password' ")
-else:
-    with open(os.path.dirname(os.path.abspath(__file__)) + '/src/file_to_upload/pcloud.credential', 'w') as pcloudfile:
-        pcloudfile.write(sys.argv[1] + "," + sys.argv[2])
+with open(os.path.dirname(os.path.abspath(__file__)) + '/src/file_to_upload/pcloud.credential', 'w') as pcloudfile:
+    pcloudfile.write(os.environ['pcloud_uname'] + "," + os.environ['pcloud_password'])
