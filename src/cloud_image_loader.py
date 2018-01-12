2e9b263191a2396af69b18a635f1d4f65196fb16
nicolalandro <nicolaxx94@live.it>
2018-01-12 09:22:14 +0100

    refactor

--- src/cloud_image_loader.py
+++ src/cloud_image_loader.py
@@ -3,13 +3,18 @@ import os
 from pcloud import PyCloud
 
 
+def loadCredentialFromFilePath(credential_path):
+    with open(credential_path, 'r') as credential_file:
+        credential_array = credential_file.read().split(',')
+    uname = credential_array[0]
+    password = credential_array[1]
+    return uname, password
+
+
 class CloudImageLoader(object):
     def __init__(self):
         credential_path = os.path.join(os.path.dirname(__file__), 'file_to_upload', "pcloud.credential")
-        with open(credential_path, 'r') as credential_file:
-            credential_array = credential_file.read().split(',')
-        uname = credential_array[0]
-        password = credential_array[1]
+        uname, password = loadCredentialFromFilePath(credential_path)
         self.pc = PyCloud(uname, password)
 
     def add_file(self, json_body):
