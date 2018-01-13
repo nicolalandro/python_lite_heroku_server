ad58bb6098ac63ed5790dcae2ff5112195530ad6
nicolalandro <nicolaxx94@live.it>
2018-01-13 11:03:20 +0100

    carica specie e immagine allo show

--- src/cloud_image_loader.py
+++ src/cloud_image_loader.py
@@ -1,6 +1,7 @@
 import json
 import os
 from pcloud import PyCloud
+from fs import opener
 
 
 def loadCredentialFromFilePath():
@@ -25,9 +26,36 @@ class CloudImageLoader(object):
         os.remove(testfile)
         return result
 
+    @property
     def get_dataset_list(self):
         lis = self._list_remote_folder()
-        return lis['metadata']['contents']
+        filename_list = lis['metadata']['contents']
+        complete_file_list = list()
+
+        uname = os.environ['pcloud_uname'].replace('@', '%40')
+        password = os.environ['pcloud_password']
+        link = 'pcloud://' + uname + ':' + password + '@/'
+        with opener.open_fs(link) as openfs:
+            i = 0
+            for file_data in filename_list:
+                file_name = file_data['name']
+                file_size = file_data['size']
+                print(file_name, " ", file_size)
+                file = openfs.openbin("/" + file_name, mode="r")
+
+                json_string = file.read(file_size).decode('utf-8').replace("'", '"')
+                json_obj = json.loads(json_string)
+                specie = json_obj['specie']
+                img = json_obj['img']
+
+                dict = {
+                    'name': file_name,
+                    'specie': specie,
+                    'img': img,
+                }
+                complete_file_list.append(dict)
+                i += 1
+        return complete_file_list
 
     def _get_progressive_number(self):
         try:
