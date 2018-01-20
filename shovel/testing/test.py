af4789d4fc34159d86e8badfdfdeddd996d3d596
nicolalandro <nicolaxx94@live.it>
2018-01-20 14:29:11 +0100

    shovel hello task

---
+++ shovel/testing/test.py
@@ -0,0 +1,12 @@
+from shovel import task
+
+
+@task
+def hello(name):
+    """
+    This is hello world.
+    Esamples:
+    shovel testing.test.hello '<name>'
+    """
+
+    print('Hello, %s' % name)
