e56312644118d465e86af8cfcb159e4260bc1e46
nicolalandro <nicolaxx94@live.it>
2018-01-30 22:36:14 +0100

    one ajax call for all image with name and size

--- static/js/show_dataset.js
+++ static/js/show_dataset.js
@@ -8,4 +8,32 @@ $(document).ready(function(){
         $('.wrapper').removeClass('list-mode');
     });
 
+    $( ".box" ).each(function( index ) {
+        var row_data = $( this );
+        var name = row_data.attr('namel');
+        var size = parseInt(row_data.attr('sizel'));
+        console.log( index + ": ajax call for " + name );
+        var data = {'name':name, 'size':size};
+         $.ajax({
+                        url: 			"/api/load_data_from_cloud",
+                        type: 			'POST',
+                        data: 			JSON.stringify(data),
+                        contentType:    "application/json; charset=utf-8",
+                        dataType:       "json",
+                        complete:       complete,
+                        success: 		success,
+                        error:          error
+                    });
+    });
+
 });
+
+function success(data){
+    console.log("success");
+};
+function complete(){
+    console.log("complete");
+};
+function error(xhr){
+    console.log("Error: " + xhr.status + ", " + xhr.statusText);
+};
