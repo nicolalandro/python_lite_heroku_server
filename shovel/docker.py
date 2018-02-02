c7a873d234b12f9bc82fcd484f5d2738a16a7709
nicolalandro <rocket@trovaprezzi.it>
2018-02-02 14:43:11 +0100

    dockerize work but do not expose to external

---
+++ shovel/docker.py
@@ -0,0 +1,37 @@
+import os
+from shovel import task
+
+@task
+def build():
+    """
+    This build docker app. Esamples: shovel docker.build
+    """
+    os.system('docker build -t flask_server .')
+
+@task
+def run_server():
+    """
+    This run docker container. Esamples: shovel docker.run_server
+    """
+    os.system('docker run -d -p 8000 flask_server')
+
+@task
+def stop_all():
+    """
+    This build stop all docker container. Esamples: shovel docker.stop_all
+    """
+    os.system('docker stop $(docker ps -a -q)')
+
+@task
+def remove_all():
+    """
+    This build remove all docker container. Esamples: shovel docker.remove_all
+    """
+    os.system('docker rm $(docker ps -a -q)')
+
+@task
+def ssh(container_id):
+    """
+    This provide a shell in docker. Esamples: shovel docker.ssh a0fa2c4438e3
+    """
+    os.system('docker exec -it ' + container_id + ' /bin/bash')
