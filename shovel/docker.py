33aeddb8b231bd42ce7d284031325c9ed4058b3c
nicolalandro <nicolaxx94@live.it>
2018-02-05 23:47:12 +0100

    shovel tasks for docker completed

--- shovel/docker.py
+++ shovel/docker.py
@@ -11,12 +11,18 @@ def build():
 
 
 @task
-def run_server():
+def run():
     """
-    This run docker container. Esamples: shovel docker.run_server
+    This run docker container. Esamples: shovel docker.run
     """
-    os.system('docker run -d -p 5000:5000 flask_server')
+    os.system('docker run -d -p 5000:5000 --name fish_service flask_server')
 
+@task
+def stop():
+    """
+    This build stop all docker container. Esamples: shovel docker.stop
+    """
+    os.system('docker stop $(docker ps -a -q -f "name=fish_service")')
 
 @task
 def stop_all():
@@ -25,6 +31,12 @@ def stop_all():
     """
     os.system('docker stop $(docker ps -a -q)')
 
+@task
+def remove():
+    """
+    This build remove all docker container. Esamples: shovel docker.remove
+    """
+    os.system('docker rm $(docker ps -a -q -f "name=fish_service")')
 
 @task
 def remove_all():
@@ -35,8 +47,16 @@ def remove_all():
 
 
 @task
-def bash(container_id):
+def bash():
     """
-    This provide a shell in docker. Esamples: shovel docker.ssh a0fa2c4438e3
+    This provide a shell in docker. Esamples: shovel docker.bash
+    """
+    os.system('docker exec -it $(docker ps -a -q -f "name=fish_service") /bin/bash')
+
+
+@task
+def bash_to(container_id):
+    """
+    This provide a shell in docker. Esamples: shovel docker.bash a0fa2c4438e3
     """
     os.system('docker exec -it ' + container_id + ' /bin/bash')
--- shovel/shovel.py
+++ shovel/shovel.py
@@ -1,13 +1,6 @@
 import os
+
 from shovel import task
-
-
-@task
-def hello(name):
-    """
-    This is hello world. Esamples: shovel testing.test.hello '<name>'
-    """
-    print('Hello, %s' % name)
 
 
 @task
