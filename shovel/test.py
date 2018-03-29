a470fb0b96c7fb689ec82bf84bae16341c8d84e9
nicolalandro <nicolaxx94@live.it>
2018-03-29 18:12:19 +0200

    shovel test.run_single ... run a single test of class

--- shovel/test.py
+++ shovel/test.py
@@ -17,3 +17,11 @@ def run_all():
     This run all test. Esamples: shovel test.run_all
     """
     os.system('python -m unittest discover -s test --pattern=*test.py 1>&2')
+
+
+@task
+def run_single(test_method_path):
+    """
+        This run all test. Esamples: shovel test.run_single acceptance.my_flask_test_case.MyFlaskTestCase.test_xxx
+    """
+    os.system('python -m unittest test.' + test_method_path)
