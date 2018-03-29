3fc009950580418629cdd58bf33ebe71cb5e0035
nicolalandro <nicolaxx94@live.it>
2018-03-29 18:05:09 +0200

    test open new tab

--- test/acceptance/my_flask_test_case.py
+++ test/acceptance/my_flask_test_case.py
@@ -29,6 +29,18 @@ class MyFlaskTestCase(LiveServerTestCase):
         self.browser.switch_to.window(self.browser.window_handles[1])
         self.assertEqual(self.browser.current_url, self.get_server_url() + '/add_supervisioned_data')
 
+    def test_open_show_dataset_in_second_tab(self):
+        self.browser.get(self.get_server_url())
+        self.browser.find_elements_by_xpath("//div[@id = 'credits']/p/a")[1].click()
+        self.browser.switch_to.window(self.browser.window_handles[1])
+        self.assertEqual(self.browser.current_url, self.get_server_url() + '/show_dataset')
+
+    def test_open_fish_ai_in_second_tab(self):
+        self.browser.get(self.get_server_url())
+        self.browser.find_elements_by_xpath("//div[@id = 'credits']/p/a")[2].click()
+        self.browser.switch_to.window(self.browser.window_handles[1])
+        self.assertEqual(self.browser.current_url, self.get_server_url() + '/fish_ai')
+
     def test_add_data_exception_without_image(self):
         self.browser.get(self.get_server_url() + '/add_supervisioned_data')
         self.browser.find_element_by_id('fish_specie').send_keys('test')
